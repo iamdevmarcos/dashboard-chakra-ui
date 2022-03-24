cb4df5bff8c1994d24b49e80d43cc1cc08eefb75
Marcos Andre <marcosdev.me@gmail.com>
2022-03-24 13:26:35 -0300

    chore: install appexcharts

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,4 +1,4 @@
-import { Flex } from "@chakra-ui/react";
+import { Flex, SimpleGrid, Box, Text } from "@chakra-ui/react";
 
 import { Header } from "../components/Header";
 import { Sidebar } from "../components/Sidebar";
@@ -10,6 +10,24 @@ export default function Dashboard() {
 
       <Flex w="100%" my="6" maxWidth={1200} mx="auto" px="6">
         <Sidebar />
+
+        <SimpleGrid
+          flex="1"
+          gap="4"
+          minChildWidth="320px"
+          alignItems="flex-start"
+        >
+          <Box p="8" bg="gray.800" borderRadius={8}>
+            <Text fontSize="lg" mb="4">
+              Inscritos da semana
+            </Text>
+          </Box>
+          <Box p="8" bg="gray.800" borderRadius={8}>
+            <Text fontSize="lg" mb="4">
+              Taxa de abertura
+            </Text>
+          </Box>
+        </SimpleGrid>
       </Flex>
     </Flex>
   );
