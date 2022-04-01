32d807713279e452b26158a8c62ec71323093302
Marcos Andre <marcosdev.me@gmail.com>
2022-04-01 14:23:24 -0300

    feat: add responsive to user create

--- src/pages/users/create.tsx
+++ src/pages/users/create.tsx
@@ -22,19 +22,19 @@ export default function CreateUser() {
       <Flex w="100%" my="6" maxWidth={1200} mx="auto" px="6">
         <Sidebar />
 
-        <Box flex="1" borderRadius={8} bg="gray.800" p="8">
+        <Box flex="1" borderRadius={8} bg="gray.800" p={["6", "8"]}>
           <Heading size="lg" fontWeight="normal">
             Criar usuário
           </Heading>
           <Divider my="6" borderColor="gray.700" />
 
           <VStack spacing="8">
-            <SimpleGrid minChildWidth="240px" spacing="8" w="100%">
+            <SimpleGrid minChildWidth="240px" spacing={["6", "8"]} w="100%">
               <Input name="name" label="Nome completo" />
               <Input name="email" label="E-mail" />
             </SimpleGrid>
 
-            <SimpleGrid minChildWidth="240px" spacing="8" w="100%">
+            <SimpleGrid minChildWidth="240px" spacing={["6", "8"]} w="100%">
               <Input name="password" type="password" label="Senha" />
               <Input
                 name="password_confirmation"
