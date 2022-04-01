63d046ecd46fc230877e1916bb7bd09b2914ba88
Marcos Andre <marcosdev.me@gmail.com>
2022-04-01 11:11:29 -0300

    feat: add responsive sidebar and context for sidebar

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,11 +1,16 @@
-import { Flex, useBreakpointValue } from "@chakra-ui/react";
+import { Flex, IconButton, Icon, useBreakpointValue } from "@chakra-ui/react";
 
 import { Logo } from "./Logo";
 import { Notifications } from "./Notifications";
 import { Profile } from "./Profile";
 import { Search } from "./Search";
 
+import { useSidebarDrawer } from "../../contexts/SidebarDrawerContext";
+import { RiMenuLine } from "react-icons/ri";
+
 export function Header() {
+  const { onOpen } = useSidebarDrawer();
+
   const isWideVersion = useBreakpointValue({
     base: false,
     lg: true,
@@ -22,6 +27,17 @@ export function Header() {
       px="6"
       align="center"
     >
+      {!isWideVersion && (
+        <IconButton
+          aria-label="Open Navigation"
+          icon={<Icon as={RiMenuLine} />}
+          fontSize="24"
+          variant="unstyled"
+          onClick={onOpen}
+          mr="2"
+        ></IconButton>
+      )}
+
       <Logo />
 
       {isWideVersion && <Search />}
---
+++ src/components/Sidebar/SidebarNav.tsx
@@ -0,0 +1,26 @@
+import { Stack } from "@chakra-ui/react";
+
+import {
+  RiDashboardLine,
+  RiContactsLine,
+  RiInputMethodLine,
+  RiGitMergeLine,
+} from "react-icons/ri";
+
+import { NavSection } from "./NavSection";
+import { NavLink } from "./NavLink";
+
+export function SidebarNav() {
+  return (
+    <Stack spacing="12" align="flex-start">
+      <NavSection title="Geral">
+        <NavLink icon={RiDashboardLine}>Dashboard</NavLink>
+        <NavLink icon={RiContactsLine}>Usuários</NavLink>
+      </NavSection>
+      <NavSection title="Automação">
+        <NavLink icon={RiInputMethodLine}>Formulários</NavLink>
+        <NavLink icon={RiGitMergeLine}>Automação</NavLink>
+      </NavSection>
+    </Stack>
+  );
+}
